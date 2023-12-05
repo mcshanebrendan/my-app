9e7e4a68d24d1ad61c81c03abbee9882b5c43f76
BM1200 <mcshanebrendan@yahoo.com>
2023-12-05 17:19:56 +0000

    adding read.js

---
+++ src/components/read.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Movies from "./movies";
+
+function Read() {
+   
+    const [data, setData] = useState([]);
+
+  useEffect(
+    ()=>{
+        
+        axios.get('http://localhost:4000/api/movies')
+        .then(
+            (response)=>{
+                setData(response.data)
+            }
+        )
+        .catch(
+            (error)=>{
+                console.log(error);
+            }
+        )
+
+    }, []
+  );
+
+  const Reload= (e)=>{
+    axios.get('http://localhost:4000/api/movies')
+        .then(
+            (response)=>{
+                setData(response.data)
+            }
+        )
+        .catch(
+            (error)=>{
+                console.log(error);
+            }
+        )
+  }
+
+    return (
+        <div>
+            <h2>Hello from Read Component!</h2>
+            <Movies myMovies={data} ReloadData={Reload}></Movies>
+        </div>
+    );
+
+}
+
+export default Read;
