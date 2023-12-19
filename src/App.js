f9d405caf0635a6f618691e921e5b46b050140a3
BM1200 <mcshanebrendan@yahoo.com>
2023-12-19 19:04:27 +0000

    adding history component and changing css

--- src/App.js
+++ src/App.js
@@ -1,52 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import Content from './components/content';
-// import Footer from './components/footer';
-// import Header from './components/header';
- import Container from 'react-bootstrap/Container';
+import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
 import Create from './components/create';
 import Read from './components/read';
 import Edit from './components/edit';
 import ReviewForm from './components/reviews';
+import History from './components/history';
 
 function App() {
-
   return (
     <BrowserRouter>
-    <div className="App">
-       <Navbar style={{backgroundColor:"greenyellow"}} >
-        <Container>
-          <Navbar.Brand  style= {{color: "darkviolet"}} href="/">Movie Database</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link style= {{color: "darkviolet"}} href="/">Home</Nav.Link>
-            <Nav.Link style= {{color: "darkviolet"}}  href="/create">Add Movie</Nav.Link>
-            <Nav.Link style= {{color: "darkviolet"}}  href="/read">Read/Edit Movie</Nav.Link>
-            <Nav.Link style={{ color: 'darkviolet' }} href="/reviews">Review a Movie!</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      <Routes>
-        {/* <Route path='/' element={<Content></Content>}></Route> */}
-        <Route path='/read' element={<Read></Read>}></Route>
-        <Route path='/create' element={<Create></Create>}></Route>
-        <Route path='/edit/:id' element={<Edit></Edit>}></Route>
-        <Route path="/reviews" element={<ReviewForm />} /> {/* New route for ReviewForm */}
-      </Routes>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <h4 style={{backgroundColor: "yellowgreen"}}><b>Brendan's Movie Database</b></h4>
-      {/* <Header></Header>
-      <Content></Content>
-      <Footer /> */}
-    </div>
+      <div className="App" style={{ backgroundColor: "#007BFF", color: "#FFF" , fontFamily: 'Roboto, sans-serif'}}>
+        <Navbar style={{ backgroundColor: "#0056b3" }}>
+          <Container>
+            {/* Update the logo style with the "Pacifico" font which I added link for in public/index.html*/}
+            <Navbar.Brand href="/" style={{ fontFamily: 'Pacifico', color: "gold" }}>Movie Database</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/" style={{ color: "#FFF" }}>Home</Nav.Link>
+              <Nav.Link href="/create" style={{ color: "#FFF" }}>Add Movie</Nav.Link>
+              <Nav.Link href="/read" style={{ color: "#FFF" }}>Read/Edit Movie</Nav.Link>
+              <Nav.Link href="/reviews" style={{ color: "#FFF" }}>Review a Movie!</Nav.Link>
+              <Nav.Link href="/history" style={{ color: "#FFF" }}>History</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <Routes>
+          <Route path='/read' element={<Read />} />
+          <Route path='/create' element={<Create />} />
+          <Route path='/edit/:id' element={<Edit />} />
+          <Route path="/reviews" element={<ReviewForm />} />
+          <Route path="/history" element={<History/>} />
+        </Routes>
+        <br />
+        <br />
+        <br />
+        <br />
+        {/* <h4 style={{ backgroundColor: "#0056b3", color: "gold" , fontFamily: 'Pacifico'}}><b>Brendan's Movie Database</b></h4> */}
+        <footer style={{ backgroundColor: "#0056b3", color: "gold" , fontFamily: 'Pacifico'}}>Brendan's Movie Database</footer>
+      </div>
     </BrowserRouter>
   );
 }
---
+++ src/components/history.js
@@ -0,0 +1,45 @@
+// I am adding images that are stored in my assets/images folder
+import film from '../assets/images/film.jpg'; 
+import lumiere from '../assets/images/lumiere.jpg'
+import mickey from '../assets/images/mickey.jpg'
+import ET from '../assets/images/ET.jpg'
+import nflix from '../assets/images/nflix.jpg'
+function History() {
+    return (
+      <div className="history-container" style= {{fontFamily: 'Roboto, sans-serif'}} >
+        <h1 className="history-title">History of Film</h1>
+        <p className="history-description"> 
+          The history of film is a captivating journey that spans over a century, filled with innovation, creativity, and cultural impact. It all began in the late 19th century when inventors and visionaries around the world started exploring the possibilities of capturing and projecting moving images. The following decades witnessed the rapid evolution of this groundbreaking medium, shaping the way we entertain, educate, and communicate.
+        </p>
+        <img src={film} alt="Film History" className="small-image" />
+        <p className="history-description">
+          The Lumière Brothers, Auguste and Louis Lumière, are often credited with pioneering the birth of cinema in 1895 when they screened their short films to a mesmerized audience in Paris. These early films featured simple scenes of everyday life, such as workers leaving a factory and a train arriving at a station. The magic of motion pictures had begun, and it quickly spread across the globe.
+        </p>
+        <img src={lumiere} alt="Lumiere" className="small-image" />
+        <p className="history-description">
+          The silent film era saw the emergence of legendary figures like Charlie Chaplin, Buster Keaton, and Greta Garbo. Filmmakers experimented with storytelling techniques and visual effects, pushing the boundaries of what could be achieved on screen. Silent films relied on expressive acting, music, and intertitles to convey emotions and narratives to audiences.
+        </p>
+        <p className="history-description">
+          The introduction of sound in the late 1920s revolutionized cinema with "talkies" becoming the new standard. This era gave rise to the Golden Age of Hollywood, marked by the dominance of major studios like MGM, Warner Bros., and Paramount. Iconic movies such as "Gone with the Wind" and "The Wizard of Oz" were produced during this time.
+        </p>
+        <img src={mickey} alt="MickeyMouse" className="small-image" />
+        <p className="history-description">
+          Post-World War II cinema witnessed significant changes with the rise of independent filmmaking and various new wave movements around the world, challenging traditional narratives and techniques. This period also saw the advent of color films becoming mainstream, adding a new dimension to cinematic storytelling.
+        </p>
+        <p className="history-description">
+        The 1970s and 1980s heralded the blockbuster era in film, revolutionizing production and marketing. Films like "Jaws" and "Star Wars" not only introduced nationwide releases and massive advertising but also set new standards in special effects and sound design. This period saw the rise of franchises and high-concept films, emphasizing visual spectacle and broad appeal. Spielberg's "E.T." exemplified this trend, blending advanced effects with universal storytelling. This era fundamentally shifted Hollywood's focus towards creating large-scale, audience-attracting cinematic experiences.
+        </p>
+        <img src={ET} alt="ET" className="small-image" />
+        <p className="history-description">
+        The late 20th and early 21st centuries marked a significant digital revolution in the film industry, radically altering film production, distribution, and exhibition. The emergence of digital filmmaking technologies enabled more efficient and creative production processes, making high-quality visual effects and animation more accessible. The internet's expansion and the advent of streaming services revolutionized film distribution, allowing viewers unprecedented access to a vast array of content from around the globe. This shift not only changed audience viewing habits but also opened new avenues for independent filmmakers to reach wider audiences. The digital era also brought challenges, such as piracy and the need for traditional cinemas to innovate to remain competitive. Despite these challenges, the digital revolution has fostered a more dynamic, diverse, and globally connected film industry.
+        </p>
+        <p className="history-description">
+          Today, film continues to evolve, with diverse voices emerging from different parts of the world, and new technologies like virtual reality opening up unprecedented possibilities for storytelling. The history of film is not just a story of technological advancement but a reflection of human creativity and cultural evolution.
+        </p>
+        <img src={nflix} alt="Netflix" className="small-image" />
+      </div>
+    );
+  }
+  
+  export default History;
+  
